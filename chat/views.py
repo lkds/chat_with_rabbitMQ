a0d3b36e91f67d57985cf8b67839940465a18c24
lkds <2576027877@qq.com>
2020-04-10 12:06:45 +0800

    rabbitmqmiddleware template

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+import chat.rabbitMQ
 
 # Create your views here.
+def login(request):
+    if (request.method == 'GET'):
+        return render(request,'chat.html')
--- rabbitMQ_chat/urls.py
+++ rabbitMQ_chat/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import chat.views as chat_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',chat_view.login),
 ]
